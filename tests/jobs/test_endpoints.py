d358edd98995640bbb4659a85319b99cd048bf59
John Carney <john@pdfta.com>
2019-05-13 17:03:36 +0100

    Create job endpoint works.

--- tests/jobs/test_endpoints.py
+++ tests/jobs/test_endpoints.py
@@ -6,7 +6,19 @@ def test_get_job(client):
 
 
 def test_create_job(client):
-    post_data = {'foo': 'bar'}
+    post_data = {
+        "job": {
+            "ids": 23,
+            "contact_email": "tom@example.com",
+            "password": "foobar",
+            "company_name": "Acme",
+            "job_title": "FE Developer",
+            "job_description": "Do FE Development",
+            "location": "Manchester",
+            "salary_min": 30000,
+            "salary_max": 50000
+            }}
+
     response = client.post('/create_job', json=post_data)
 
     assert b'Job created' in response.data
